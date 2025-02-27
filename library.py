3e395384636a5526ef78bb1f55f338c5ab27fe19
Heitor <161908829+Heitor-M33n@users.noreply.github.com>
2025-02-27 10:28:37 -0300

    Update library.py

--- library.py
+++ library.py
@@ -2,69 +2,71 @@ import os, csv, time
 import library_clientes_e_filiais as cf
 import library_estoque_e_vendas as ev
 
-valores = ['id do cliente', 'nome completo', 'telefone', 'cpf'] #todos os dados são do tipo str ou int, exceto compras -> list[str], com a informação sobre a compra e seu horário ( usar datetime ) 
-type_valores = ['int', 'str', 'int', 'int']
-produtos_e_valores = [[], []]
-
 def comandos_p1():
     time.sleep(1)
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print('/help - Mostra os comandos') #done
-    print('/sair - Fecha o sistema') #done
-    print('/vis_dados - Mostra todos os dados de clientes') #done
-    print('/vis_dados_filial - Mostra os dados de uma filial de escolha') #done
-    print('/add_filial - Cria uma filial vazia ( e um estoque para ela )') #melhorar e consertar
-    print('/del_filial - Remove uma filial, deletando seus dados ou os movendo') #done
-    print('/rename_filial - Renomeia uma filial de escolha') #done
-    print("  1/2 digite '/' e número da página que deseja acessar ( como /2 )")
+    print('/help - Mostra os comandos')
+    print('/sair - Fecha o sistema')
+    print('/vis_dados - Mostra todos os dados de clientes')
+    print('/vis_dados_filial - Mostra os dados de uma filial de escolha')
+    print('/add_filial - Cria uma filial vazia ( e um estoque para ela )')
+    print('/del_filial - Remove uma filial, deletando seus dados ( e seu estoque )')
+    print('/rename_filial - Renomeia uma filial de escolha')
+    print('  1/3 digite / e número da página que deseja acessar ( como /2 )')
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 def comandos_p2():
     time.sleep(1)
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print('/add_cliente - Adiciona cliente(s) à filial de escolha') #testar
-    print('/del_cliente - Remove cliente(s) de uma filial') #testar
-    print('/find_cliente - Localiza cliente e seus dados por alguma informação dele.')
+    print('/add_cliente - Adiciona cliente(s) à filial de escolha')
+    print('/find_cliente - Localiza cliente por alguma informação dele.')
+    print('/del_cliente - Remove cliente(s) de uma filial')
     print('/alterar_dados - Altera dados de escolha de um cliente')
-    print('/list_filiais - Mostra todas as filiais existentes') #done
-    print("  2/2 ( digite / e número da página que deseja acessar ( como /1 )")
+    print('/list_filiais - Mostra todas as filiais existentes')
+    print('  2/3 digite / e número da página que deseja acessar')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+def comandos_p3():
+    time.sleep(1)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('  3/3 digite / e número da página que deseja acessar')
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 def main_loop():
     while True:
         info = get_info()
-        clientes, filiais, estoque, valores = info
-        print(info)
+        clientes, filiais, estoque, valores_totais = info
+        tipos_de_dado, type_dados, produtos, precos = valores_totais
 
         inp = (input('\n').strip()).lower()
         print()
         
-        if inp == '/help' or inp == '/1': #done
+        if inp == '/help' or inp == '/1':
             comandos_p1()
-        elif inp == '/2': #done
+        elif inp == '/2':
             comandos_p2()
-        elif inp == '/sair': #done
+        elif inp == '/sair':
             print('Saindo do sistema...')
             return
-        elif inp == '/vis_dados': #done
-            cf.visualizar_dados(clientes, valores, filiais)
-        elif inp == '/vis_dados_filial': #done
-            cf.visualizar_dados_filial(clientes, valores, filiais)
-        elif inp == '/add_filial': #melhorar e consertar
+        elif inp == '/vis_dados':
+            cf.visualizar_dados(clientes, tipos_de_dado, filiais)
+        elif inp == '/vis_dados_filial':
+            cf.visualizar_dados_filial(clientes, tipos_de_dado, filiais)
+        elif inp == '/add_filial':
             cf.adicionar_filial(clientes, filiais)
-        elif inp == '/del_filial': #done
+        elif inp == '/del_filial':
             cf.remover_filial(clientes, filiais)
-        elif inp == '/rename_filial': #done
+        elif inp == '/rename_filial':
             cf.rename_filial(clientes, filiais)
-        elif inp == '/add_cliente': #testar
-            cf.adicionar_cliente(clientes, valores, filiais, type_valores)
-        elif inp == '/del_cliente': #testar
-            cf.remover_cliente(clientes)
+        elif inp == '/add_cliente':
+            cf.adicionar_cliente(clientes, tipos_de_dado, filiais, type_dados)
+        elif inp == '/del_cliente':
+            cf.remover_cliente(clientes, filiais)
         elif inp == '/find_cliente':
-            cf.encontrar_cliente()
+            cf.encontrar_cliente(clientes, filiais, valores_totais)
         elif inp == '/alterar_dados':
             cf.alterar_dados_cliente()
-        elif inp == '/list_filiais': #done
+        elif inp == '/list_filiais':
             print([x[0:-4] for x in filiais])
         elif inp == '/novo_tipo_de_dado':
             cf.novo_dado()
@@ -87,6 +89,10 @@ def get_info() -> list[list[list[list[str]]], list[str], list[str], list]:
             for linha in csv.reader(file):
                 filial.append(linha)
         clientes.append(filial)
+    
+    with open(f'valores.csv', mode='r', newline='') as file:
+        for linha in csv.reader(file):
+            valores.append(linha)
 
     return [clientes, filiais, estoque, valores]
    
@@ -105,9 +111,12 @@ def remover_caracteres(string: str, modo: str='num') -> int:
             if not i.isspace():
                 string_new += i
 
-    return string_new
+    string_new.strip()
+    if string_new:
+        return string_new
+    return False
 
-def escrever_csv(clientes: list[list[list]], filiais: list[str], modo: str='w', valores: list=0): #done
+def escrever_csv(clientes: list[list[list]], filiais: list[str], modo: str='w'): #done
         if modo == 'w':
             for i in filiais:
                 with open(f'filiais/{i}', mode='w', newline='') as file:
@@ -120,3 +129,4 @@ def escrever_csv(clientes: list[list[list]], filiais: list[str], modo: str='w', 
                 return
         elif modo == 'valores':
             with open('valores.csv', mode='w', newline='') as file:
+                return
